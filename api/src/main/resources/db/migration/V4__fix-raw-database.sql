029a8db9f09b6cacc10395b8751ec6eda13aadcc
rodsouzab <rodrigosouza.souzabarbosa1@gmail.com>
2025-04-11 16:11:08 -0300

    add columns coordenador and registro

---
+++ api/src/main/resources/db/migration/V4__fix-raw-database.sql
@@ -0,0 +1,22 @@
+create table coordenador (
+                             cpf_pessoa varchar(11) primary key,
+                             foreign key(cpf_pessoa) references pessoa(cpf)
+);
+
+create table coordenador_equipe (
+                                    id_equipe int,
+                                    cpf_coordenador varchar(11),
+                                    primary key(id_equipe, cpf_coordenador),
+                                    foreign key(id_equipe) references equipe(id),
+                                    foreign key(cpf_coordenador) references coordenador(cpf_pessoa)
+);
+
+create table registro_encontreiro (
+                                      id_equipe int,
+                                      cpf_encontreiro varchar(11),
+                                      ano_encontro int,
+                                      primary key(id_equipe, cpf_encontreiro, ano_encontro),
+                                      foreign key(id_equipe) references equipe(id),
+                                      foreign key(cpf_encontreiro) references encontreiro(cpf_pessoa),
+                                      foreign key(ano_encontro) references encontro(ano)
+);
